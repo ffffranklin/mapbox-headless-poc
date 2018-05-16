ccea2856d244f1b5794e882c547b69ed39354ad5
Franklin Clark <franklin@modeanalytics.com>
2018-05-16 14:11:32 -0700

    Add PDF export support and logging

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const serveStatic = require('serve-static');
 const puppeteer = require('puppeteer');
 
 const paths = {
-  screenshot: 'screenshot.png',
+  imageExport: 'screenshot.png',
+  pdfExport: 'screenshot.pdf',
 };
 const urls = {
   poc: 'http://localhost:8080',
@@ -11,18 +12,19 @@ const urls = {
 };
 
 const port = 8080;
-const pageTimeout = 5000;
+const pageLoadTimeout = 10000;
+const captureDelay = 5000;
 const app = connect().use(serveStatic(__dirname));
 
 const server = app.listen(port, function(){
   console.log('Server running on 8080...');
 });
 
-async function run() {
+async function capture(targetUrl, imageExportPath, pdfExportPath) {
   try {
     // necessary to work with gpu
     // inspiredby: https://github.com/GoogleChrome/puppeteer/issues/1260#issuecomment-348878456
-    const browser = await puppeteer.launch({
+    const opts = {
       // prevents puppeteer from setting headless flags
       headless: false,
       // so we can set them here without --disable-gpu
@@ -31,19 +33,45 @@ async function run() {
         '--hide-scrollbars',
         '--mute-audio'
       ]
-    });
+    };
+
+    const browser = await puppeteer.launch(opts);
     const page = await browser.newPage();
 
-    await page.goto(urls.poc);
-    await page.waitFor(pageTimeout);
-    await page.screenshot({path: paths.screenshot});
+    console.log('Navigating to "%s"', targetUrl);
+    await page.goto(targetUrl, {
+      timeout: pageLoadTimeout,
+      waitUntil: 'networkidle2',
+    });
+
+    console.log('Waiting for page to render');
+    await page.waitFor(captureDelay);
+
+    console.log('Capturing image screenshot to "%s"', imageExportPath);
+    await page.screenshot({
+      path: imageExportPath ,
+    });
+
+    console.log('Capturing pdf screenshot to "%s"', pdfExportPath);
+    await page.pdf({
+      path: pdfExportPath,
+      format: 'letter',
+    });
+
+    console.log('Closing page process');
+    await page.close();
+
+    console.log('Closing browser process');
     await browser.close();
   } catch(e) {
     console.error(e);
   } finally {
+    console.log('Closing server');
     server.close(()=> console.log('Server closed!'))
   }
 }
 
 /** __main__ **/
-run();
+capture(urls.poc, paths.imageExport, paths.pdfExport)
+  .then(()=> console.log('Done'))
+  .catch((e)=> console.error(e));
